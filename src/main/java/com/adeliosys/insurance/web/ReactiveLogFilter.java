c6c959596aa874cee4dac4b069f101f7682d63c9
Florian Beaufumé <fbeaufume@gmail.com>
2018-10-13 17:52:19 +0200

    Bypassed WebFilter status late assignment issue

--- src/main/java/com/adeliosys/insurance/web/ReactiveLogFilter.java
+++ src/main/java/com/adeliosys/insurance/web/ReactiveLogFilter.java
@@ -2,6 +2,7 @@ package com.adeliosys.insurance.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -25,11 +26,16 @@ public class ReactiveLogFilter implements WebFilter {
 
         LOGGER.info("Serving {} '{}'", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
 
-        return chain.filter(exchange).doAfterTerminate(() -> LOGGER.info("Served {} '{}' as {} in {} msec",
-                exchange.getRequest().getMethod(),
-                exchange.getRequest().getURI(),
-                // TODO FBE status code is null, see org.springframework.http.server.reactive.AbstractServerHttpResponse:get/setStatusCode
-                exchange.getResponse().getStatusCode(),
-                System.currentTimeMillis() - timestamp));
+        return chain.filter(exchange).doAfterTerminate(() -> {
+            HttpStatus status = exchange.getResponse().getStatusCode();
+            LOGGER.info("Served {} '{}' as {} in {} msec",
+                    exchange.getRequest().getMethod(),
+                    exchange.getRequest().getURI(),
+                    // WebFlux sets the default 200 status later than this callback,
+                    // see https://stackoverflow.com/questions/52668050/spring-webflux-statuscode-is-null-in-a-webfilter
+                    // and https://jira.spring.io/browse/SPR-17368
+                    status == null ? 200 : status.value(),
+                    System.currentTimeMillis() - timestamp);
+        });
     }
 }
